00ecac419fe6435c3365c619d94e6ff0ba8237b2
Flaming <flamingxo@gmail.com>
2022-03-01 21:15:31 -0700

    + Update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-const { Client, Intents, Collection } = require("discord.js");
-const config = require("./config/config.json");
-const client = new Client({intents: [
-    Intents.FLAGS.GUILDS, 
-    Intents.FLAGS.GUILD_MESSAGES, 
-    Intents.FLAGS.GUILD_BANS, 
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-]});
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config/config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
-client.aliases = new Collection();
-["command", "event"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
-});
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-client.login(config.token);
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(token);
